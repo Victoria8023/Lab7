c4a11b798e84df723a5962691f5b316221293c22
Victoria8023 <yuxuanl4@illinois.edu>
2018-03-06 14:49:02 -0600

    not finished

--- src/main/java/MorseDecoder.java
+++ src/main/java/MorseDecoder.java
@@ -25,7 +25,6 @@ import java.util.Scanner;
  * each symbol in a table and return the corresponding letter value.
  */
 public class MorseDecoder {
-
     /**
      * Bin size for power binning. We compute power over bins of this size. You will probably not
      * need to modify this value.
@@ -52,7 +51,9 @@ public class MorseDecoder {
         double[] returnBuffer = new double[totalBinCount];
 
         double[] sampleBuffer = new double[BIN_SIZE * inputFile.getNumChannels()];
+        int count = 0;
         for (int binIndex = 0; binIndex < totalBinCount; binIndex++) {
+            sampleBuffer[binIndex] = inputFile.readFrames(sampleBuffer, BIN_SIZE);
             // Get the right number of samples from the inputFile
             // Sum all the samples together and store them in the returnBuffer
         }
@@ -77,6 +78,11 @@ public class MorseDecoder {
      * @return the Morse code string of dots, dashes, and spaces
      */
     private static String powerToDotDash(final double[] powerMeasurements) {
+        for (int i = 0; i < powerMeasurements.length; i++) {
+            if (powerMeasurements[i] >= POWER_THRESHOLD) {
+                
+            }
+        }
         /*
          * There are four conditions to handle. Symbols should only be output when you see
          * transitions. You will also have to store how much power or silence you have seen.
